cf3ce6676e68f496e03a80f7e969384efa2568ec
22300742 <florian@bouchery.me>
2025-02-17 10:38:02 +0100

    Modification des fichiers de configurations pour junit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.deuxiemeprojet {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires junit;
 
     opens com.example.deuxiemeprojet to javafx.fxml;
     exports modele;
