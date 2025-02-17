266d6799daacc91064252520cfd6aef7580d961f
22300742 <florian@bouchery.me>
2025-02-17 10:17:02 +0100

    Réparation des problèmes d'implémentation des tests d'Ajout

--- src/main/java/tests/TestAjout.java
+++ src/main/java/tests/TestAjout.java
@@ -1,46 +1,40 @@
 package tests;
 
 import junit.framework.*;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.*;
-import src.java.com.modele.*;
+import modele.*;
 
 public class TestAjout extends TestCase {
 
-	@Test
-	public void TestParReservInvalide() throws ExceptionPlanning {
+	public void testParReservInvalide() {
 				
 		try{
-			PlanningCollection plan = new PlanningCollection();
+			PlanningCollections plan = new PlanningCollections();
 			plan.ajout(new Reservation(new DateCalendrier(30, 12, 1582), new PlageHoraire(new Horaire(8, 15), new Horaire(9, 30)), "test", "testNiveau"));
     	}
     	catch(ExceptionPlanning e){
-    		System.out.println(ConstantesErreur.ERREURS_PLANNING[e.getCodeErreur]);
+    		System.out.println(ConstantesErreur.ERREURS_PLANNING[e.getCodeErreur()]);
     	}
 	}
-	
-	@Test
-	public void TestToutValide() throws ExceptionPlanning {
+
+	public void testToutValide() {
 		try{
-			PlanningCollection plan = new PlanningCollection();
+			PlanningCollections plan = new PlanningCollections();
 			plan.ajout(new Reservation(new DateCalendrier(30, 12, 2024), new PlageHoraire(new Horaire(8, 15), new Horaire(9, 30)), "test", "testNiveau"));
 			plan.ajout(new Reservation(new DateCalendrier(30, 12, 2024), new PlageHoraire(new Horaire(10, 15), new Horaire(11, 0)), "testBis", "testNiveauBis"));
 		}
 		catch(ExceptionPlanning e){
-    		System.out.println(ConstantesErreur.ERREURS_PLANNING[e.getCodeErreur]);
+    		System.out.println(ConstantesErreur.ERREURS_PLANNING[e.getCodeErreur()]);
     	}
     }
-    
-    @Test
-	public void TestReservIncompatible() throws ExceptionPlanning {
+
+	public void testReservIncompatible() {
 		try{
-			PlanningCollection plan = new PlanningCollection();
+			PlanningCollections plan = new PlanningCollections();
 			plan.ajout(new Reservation(new DateCalendrier(30, 12, 2024), new PlageHoraire(new Horaire(8, 15), new Horaire(9, 30)), "test", "testNiveau"));
 			plan.ajout(new Reservation(new DateCalendrier(30, 12, 2024), new PlageHoraire(new Horaire(9, 15), new Horaire(11, 0)), "testBis", "testNiveauBis"));
 		}
 		catch(ExceptionPlanning e){
-    		System.out.println(ConstantesErreur.ERREURS_PLANNING[e.getCodeErreur]);
+    		System.out.println(ConstantesErreur.ERREURS_PLANNING[e.getCodeErreur()]);
     	}
     }
 }
